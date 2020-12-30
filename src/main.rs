03687d0f61c399e7975a4d1f42a02ae78ff3da6e
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-30 16:24:05 +0900

    feat:error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use actix_web::{get, App, HttpResponse, HttpServer};
+use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use thiserror::Error;
 
+// エラーハンドリングの定義
+// エラーをまとめる enum を定義
+// actix_web::ResponseError として使うために derive マクロで Debug を付与している必要があります
+#[derive(Error, Debug)]
+enum MyError {}
+
+// actix_web::ResponseError を MyError に実装する
+// 今回はデフォルトの実装をそのまま使うので、あたらに実装するものはない
+impl ResponseError for MyError {}
+
+// MyError は actix_web::ResponseError を実装しているので index の戻り値に MyError を使うことができる
+// path
 #[get("/")]
 async fn index() -> Result<HttpResponse, actix_web::Error> {
     let response_body = "Hello world!";
@@ -11,6 +24,7 @@ async fn index() -> Result<HttpResponse, actix_web::Error> {
     Ok(HttpResponse::Ok().body(response_body))
 }
 
+// サーバーの起動
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
     HttpServer::new(move || App::new().service(index))
@@ -19,3 +33,5 @@ async fn main() -> Result<(), actix_web::Error> {
         .await?;
     Ok(())
 }
+
+
