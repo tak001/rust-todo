772e44d67314ddd3697baf2470930f1a78cb63af
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-30 18:00:12 +0900

    feat:DB

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,72 @@
-use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::params;
 use thiserror::Error;
-
-// エラーハンドリングの定義
-// エラーをまとめる enum を定義
-// actix_web::ResponseError として使うために derive マクロで Debug を付与している必要があります
-#[derive(Error, Debug)]
-enum MyError {}
-
-// actix_web::ResponseError を MyError に実装する
-// 今回はデフォルトの実装をそのまま使うので、あたらに実装するものはない
-impl ResponseError for MyError {}
-
-// MyError は actix_web::ResponseError を実装しているので index の戻り値に MyError を使うことができる
-// path
-#[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
-    let response_body = "Hello world!";
-
-    //HttpResponse::Ok() はステータスコード200 を持つ HttpResponseBuilder という構造体を返す
-    // HttpResponseBuilder の body() という関数にレスポンスのボディを渡すと HttpResponse が返ってくる
-    // 戻り値の方が　Result なので Ok で包む
-
-    Ok(HttpResponse::Ok().body(response_body))
+struct TodoEntry {
+    id: u32,
+    text: String,
 }
 
-// サーバーの起動
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConncectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
+}
+
+impl ResponseError for MyError {}
+
+#[get("/")]
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    let mut statement = conn.prepare("SELECT id, text FROM todo")?;
+    let rows = statement.query_map(params![], |row| {
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(TodoEntry { id, text })
+    })?;
+
+    let mut entries = Vec::new();
+    for row in rows {
+        entries.push(row?);
+    }
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
+}
+
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
-    HttpServer::new(move || App::new().service(index))
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
+    let conn = pool
+        .get()
+        .expect("Failed to get the connection from the pool.");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todo (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        )",
+        params![],
+    )
+    .expect("Failed to create a table `todo`.");
+    HttpServer::new(move || App::new().service(index).data(pool.clone()))
         .bind("0.0.0.0:8080")?
         .run()
         .await?;
     Ok(())
 }
-
-
