60eb59177c7bda4431d298b22dfbe04ddaaa9772
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-30 16:05:37 +0900

    feat:serve

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, actix_web::Error> {
+    let response_body = "Hello world!";
+
+    //HttpResponse::Ok() はステータスコード200 を持つ HttpResponseBuilder という構造体を返す
+    // HttpResponseBuilder の body() という関数にレスポンスのボディを渡すと HttpResponse が返ってくる
+    // 戻り値の方が　Result なので Ok で包む
+
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[actix_rt::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+    Ok(())
 }
